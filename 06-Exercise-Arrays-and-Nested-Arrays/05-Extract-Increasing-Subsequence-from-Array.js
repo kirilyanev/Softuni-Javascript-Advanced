ed8f647cd764b77ad650fa800cd9e6b29fda0508
Kiril Yanev <kl_yanev@abv.bg>
2023-01-15 13:04:47 +0200

    05. Extract Increasing Subsequence from Array

---
+++ 06-Exercise-Arrays-and-Nested-Arrays/05-Extract-Increasing-Subsequence-from-Array.js
@@ -0,0 +1,32 @@
+function extractSequence(arr) {
+    let result = [];
+    let biggestNum = arr[0];
+    const arrLength = arr.length;
+
+    for (let i = 0; i < arrLength; i++) {
+        let currNum = arr[i];
+        if (currNum >= biggestNum) {
+            result.push(currNum);
+            biggestNum = currNum;
+        }
+    }
+
+    return result;
+}
+
+console.log(extractSequence([1,
+    3,
+    8,
+    4,
+    10,
+    12,
+    3,
+    2,
+    24]));
+console.log(extractSequence([20, 
+    3, 
+    2, 
+    15,
+    6, 
+    1]
+    ));
