2397a82d5e1001b29e72680b75ae22156b780c82
Kiril Yanev <kl_yanev@abv.bg>
2023-01-16 21:28:25 +0200

    06-08 Sort an array by 2 criteria
    Simpler solution

--- 06-Exercise-Arrays-and-Nested-Arrays/08-Sort-an-Array-by-2-Criteria.js
+++ 06-Exercise-Arrays-and-Nested-Arrays/08-Sort-an-Array-by-2-Criteria.js
@@ -1,14 +1,17 @@
 function sortArr(arr) {
-    arr.sort((a, b) => {
-        if (a.length !== b.length) {
-            return a.length - b.length;
-        } else {
-            return a.toLowerCase().localeCompare(b.toLowerCase());
-        }
-    });
-    for (let element of arr) {
-        console.log(element);
-    }
+    // arr.sort((a, b) => {
+    //     if (a.length !== b.length) {
+    //         return a.length - b.length;
+    //     } else {
+    //         return a.toLowerCase().localeCompare(b.toLowerCase());
+    //     }
+    // });
+    // for (let element of arr) {
+    //     console.log(element);
+    // }
+    
+    arr.sort((a,b)=> a.length - b.length || a.localeCompare(b));
+    console.log(arr.join('\n'));
 }
 
 sortArr(['alpha',
