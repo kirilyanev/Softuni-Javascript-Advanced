d3299165acd3209c50dce92a4ab652cb553ca0be
Kiril Yanev <kl_yanev@abv.bg>
2023-01-18 15:39:09 +0200

    Update-03-City taxes

--- 07-Objects-and-Composition/03-City-Taxes.js
+++ 07-Objects-and-Composition/03-City-Taxes.js
@@ -12,7 +12,7 @@ function cityTaxes(name, population, treasury) {
             this.population += Math.floor(this.population * percentage / 100);
         },
         applyRecession(percentage) {
-            this.treasury -= Math.floor(this.treasury * percentage / 100);
+            this.treasury -= Math.ceil(this.treasury * percentage / 100);
         }
     };
     return result;
