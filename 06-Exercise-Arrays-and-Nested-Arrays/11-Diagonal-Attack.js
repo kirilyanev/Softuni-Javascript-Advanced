de97fe9b46b0edf53b06cb84feef1f820f3313c2
Kiril Yanev <kl_yanev@abv.bg>
2023-01-20 23:03:07 +0200

    Update 11-Diagonal-Attack
    Solution working 100/100 in judge

--- 06-Exercise-Arrays-and-Nested-Arrays/11-Diagonal-Attack.js
+++ 06-Exercise-Arrays-and-Nested-Arrays/11-Diagonal-Attack.js
@@ -1,54 +1,34 @@
 function checkDiagonals(input) {
-    let equalMatrix = input.slice().map(row => row.split(' ').map(el => el = false));
-    let matrix = input.map(row => row.split(' '));
+    let matrix = input.map(row=>row.split(' '));
 
-    let diagonalOne = 0;
-    let diagonalTwo = 0;
-    // set diagonal two column start index
-    let colTwo = matrix[0].length - 1;
+    let firstDiagonal = 0;
+    let secondDiagonal = 0;
 
-    // Check if diagonals are equal
-    for (let i = 0; i < matrix.length; i++) {
-        let rowOne = i;
-        let colOne = i;
-        let rowTwo = i;
-
-        diagonalOne += Number(matrix[rowOne][colOne]);
-        diagonalTwo += Number(matrix[rowTwo][colTwo]);
-
-        equalMatrix[rowOne][colOne] = matrix[rowOne][colOne];
-        equalMatrix[rowTwo][colTwo] = matrix[rowTwo][colTwo];
-        colTwo--;
+    for (let i = 0; i < input.length; i++) {
+        firstDiagonal += Number(matrix[i][i]);
+        secondDiagonal += Number(matrix[i][matrix.length - 1 - i]);
     }
-
-    //if not equal
-    if (diagonalOne !== diagonalTwo) return console.log((matrix.map(row => row.join(' '))).join('\n'));
-
-    equalMatrix = equalMatrix.map(row => row.map(el => {
-        if (el == false) {
-            return diagonalOne;
+    
+    if (firstDiagonal !== secondDiagonal) {
+        console.log((matrix.map(row=>row.join(' '))).join('\n'));
+    } else {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix.length; j++) {
+                if(j !== matrix.length - 1 - i && j !== i) {
+                    matrix[i][j] = firstDiagonal;
+                }
+            }
         }
-        return el;
-    }));
-
-    console.log((equalMatrix.map(row => row.join(' '))).join('\n'));
+        console.log((matrix.map(row=>row.join(' '))).join('\n'));
+    }
 }
 
-checkDiagonals([
-    '5 3 12 3 1',
-    '11 4 23 2 5',
-    '101 12 8 21 10',
-    '1 4 5 2 2',
-    '5 22 33 11 1']);
-
-    console.log('-----------------');
+checkDiagonals(['5 3 12 3 1',
+                '11 4 23 2 5',
+                '101 12 3 21 10',
+                '1 4 5 2 2',
+                '5 22 33 11 1']);
 
 checkDiagonals(['1 1 1',
-    '1 1 1',
-    '1 1 0']);
-
-console.log('-----------------');
-
-checkDiagonals([
-    '5 4',
-    '3 5']);
+                '1 1 1',
+                '1 1 0']);
