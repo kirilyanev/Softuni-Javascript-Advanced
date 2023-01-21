228bea0cd39d02c8bfb3f1e12762c875d904434e
Kiril Yanev <kl_yanev@abv.bg>
2023-01-21 20:50:48 +0200

    08-Rectangle

---
+++ 08-Objects-and-Composition/08-Rectangle.js
@@ -0,0 +1,19 @@
+function rectangle(width,height,color) {
+    let finalColor = color[0].toUpperCase()+color.slice(1);
+    const rect = {
+        width,
+        height,
+        color: finalColor,
+        calcArea(){
+            let result = this.width * this.height;
+            return result;
+        }
+    }
+    return rect;
+}
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
