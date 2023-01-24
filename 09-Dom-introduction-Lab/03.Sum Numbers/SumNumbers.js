834cf14bfc0984244c5508562d6c3837ec2409c6
Kiril Yanev <kl_yanev@abv.bg>
2023-01-24 11:44:38 +0200

    03-Sum numbers

---
+++ 09-Dom-introduction-Lab/03.Sum Numbers/SumNumbers.js
@@ -0,0 +1,13 @@
+function calc() {
+    // TODO: sum = num1 + num2
+    let firstNumElement = document.getElementById('num1');
+    let secondNumElement = document.getElementById('num2');
+
+    let firstNumber = Number(firstNumElement.value);
+    let secondNumber = Number(secondNumElement.value);
+
+    let sum = firstNumber + secondNumber;
+
+    let resultElement = document.getElementById('sum');
+    resultElement.value = sum;
+}
