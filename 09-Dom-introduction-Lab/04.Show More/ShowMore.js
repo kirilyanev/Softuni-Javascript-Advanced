aaa293f4bb8d6f65d56bbfebda24fc1cb5260371
Kiril Yanev <kl_yanev@abv.bg>
2023-01-24 11:52:49 +0200

    04-Show more

---
+++ 09-Dom-introduction-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    let hiddenTextElement = document.getElementById('text');
+    let readMoreElement = document.getElementById('more');
+
+    readMoreElement.style = 'display:none';
+    hiddenTextElement.style = 'display:inline';
+}
