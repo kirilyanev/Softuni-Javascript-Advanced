34833d258e2021604ccb6316da4a78bb3c227428
Kiril Yanev <kl_yanev@abv.bg>
2023-01-26 17:17:37 +0200

    Update 06-Format the Text
    Adding second solution

--- 10-Dom-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 10-Dom-Introduction-Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,20 +1,34 @@
 function solve() {
-  let inputElement = document.getElementById('input');
-  let text = inputElement.value.split('.').filter(s => s !== '');
-  let paragraph = '';
-  let divElement = document.getElementById('output');
+  // First solution with for cycle
+  // let inputElement = document.getElementById('input');
+  // let text = inputElement.value.split('.').filter(s => s !== '');
+  // let paragraph = '';
+  // let divElement = document.getElementById('output');
 
-  for (let i = 0; i < text.length; i += 3) {
-    let firstSentence = text[i] || '';
-    let secondSentence = text[i + 1] || '';
-    let thirdSentence = text[i + 2] || '';
+  // for (let i = 0; i < text.length; i += 3) {
+  //   let firstSentence = text[i] || '';
+  //   let secondSentence = text[i + 1] || '';
+  //   let thirdSentence = text[i + 2] || '';
 
-    paragraph += firstSentence + secondSentence + thirdSentence + '.';
+  //   paragraph += firstSentence + secondSentence + thirdSentence + '.';
 
+  //   let p = document.createElement("p");
+  //   p.textContent = paragraph;
+  //   divElement.appendChild(p);
+
+  //   paragraph = '';
+  // }
+
+  // Cleaner solution with while cycle
+
+  let inputTextElement = document.getElementById('input');
+  let textArr = inputTextElement.value.split('.').filter(s => s !== '');
+  let resultDiv = document.getElementById('output');
+
+  while (textArr.length > 0) {
+    let text = textArr.splice(0,3).join('.') + '.';
     let p = document.createElement("p");
-    p.textContent = paragraph;
-    divElement.appendChild(p);
-
-    paragraph = '';
+    p.textContent = text;
+    resultDiv.appendChild(p);
   }
 }
