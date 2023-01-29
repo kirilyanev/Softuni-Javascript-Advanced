66353e3547838f2f81ebab926a9a275a315f1b69
Kiril Yanev <kl_yanev@abv.bg>
2023-01-29 17:10:46 +0200

    02-Delete from table

--- 11-DOM-Manipulations-and-Events-Lab/02. Delete-From-Table/app.js
+++ 11-DOM-Manipulations-and-Events-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputText = document.getElementsByTagName('input')[0].value;
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    let resultElement = document.querySelector('#result');
+
+    for (let i = 0; i < rows.length; i++) {
+        let tdElements = Array.from(rows[i].querySelectorAll(':nth-child(2)'));
+        tdElements.forEach(td => {
+            if (td.textContent === inputText) {
+                let row = td.parentNode;
+                row.parentNode.removeChild(row);
+                resultElement.textContent = "Deleted.";
+            } else {
+                resultElement.textContent = "Not found.";
+            }
+        });
+    }
 }
