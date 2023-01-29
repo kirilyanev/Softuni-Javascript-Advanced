dccd95ff701e280ab6a33d03d3156028661e4f15
Kiril Yanev <kl_yanev@abv.bg>
2023-01-29 21:35:57 +0200

    05-Highlight-Active

--- 11-DOM-Manipulations-and-Events-Lab/05. Highlight-Active/app.js
+++ 11-DOM-Manipulations-and-Events-Lab/05. Highlight-Active/app.js
@@ -1,3 +1,16 @@
 function focused() {
-    console.log('TODO:...');
+    let inputElements = Array.from(document.querySelectorAll('div input'));
+    
+    for (const input of inputElements) {
+        input.addEventListener('focus', focus);
+        input.addEventListener('blur', blur);
+    }
+
+    function focus(e) {
+        e.target.parentNode.classList = 'focused';
+    }
+
+    function blur(e) {
+        e.target.parentNode.classList = '';
+    }
 }
