42e806368564f83d10b8f26893009dcec5fd04cf
Kiril Yanev <kl_yanev@abv.bg>
2023-01-29 22:35:34 +0200

    06-Dynamic Validation

--- 11-DOM-Manipulations-and-Events-Lab/06. Dynamic-Validation/app.js
+++ 11-DOM-Manipulations-and-Events-Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.querySelector('#email');
+    inputElement.addEventListener('change', changeValidate);
+    inputElement.addEventListener('focus', null);
+
+    let regExValidator = /([a-z]+@[a-z]+.[a-z]+)/gm;
+
+    function changeValidate() {
+        let email = inputElement.value;
+        let isValid = email.match(regExValidator);
+
+        if (!isValid) {
+            inputElement.classList = 'error';
+        } else {
+            inputElement.classList = '';
+        }
+    }
 }
