e404b002804bc10109b8b09c96e005ee7653ada3
Kiril Yanev <kl_yanev@abv.bg>
2023-01-29 20:15:33 +0200

    03-Add-Delete

--- 11-DOM-Manipulations-and-Events-Lab/03. Add-Delete/app.js
+++ 11-DOM-Manipulations-and-Events-Lab/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    let itemsElement = document.querySelector('#items');
+    let inputElement = document.querySelector('#newItemText');
+    
+    let liElement = document.createElement('li');
+    liElement.textContent = inputElement.value;
+
+    inputElement.value = '';
+
+    let deleteElement = document.createElement('a');
+
+    deleteElement.textContent = '[Delete]';
+    deleteElement.href = "#"
+    //deleteElement.onclick = deleteElement;
+
+    liElement.appendChild(deleteElement);
+    itemsElement.appendChild(liElement);
+    
+    // function deleteElement(e) {
+    //     let elementToDelete = e.target.parentNode.remove();
+    // }
+
+    deleteElement.addEventListener('click', (e)=> {
+        e.currentTarget.parentNode.remove();
+    })
 }
