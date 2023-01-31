c7b0346098aeecbf16394355490698b1e2337a09
Kiril Yanev <kl_yanev@abv.bg>
2023-01-31 20:30:17 +0200

    05-Encode and Decode Messages

--- 12-Exercise-DOM-Manipulations-and-Events/05. Encode-and-Decode-Messages/app.js
+++ 12-Exercise-DOM-Manipulations-and-Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,24 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textareaElements = document.querySelectorAll('textarea');
+    let btnElements = document.querySelectorAll('button');
+    let receivedMessage = '';
+
+    btnElements[0].addEventListener('click', encodeMsg);
+    btnElements[1].addEventListener('click', result);
+
+    function encodeMsg() {
+        let encodedMessage = '';
+        receivedMessage = textareaElements[0].value;
+
+        for (const char of receivedMessage) {
+            let encodedChar = char.charCodeAt() + 1;
+            encodedMessage += String.fromCharCode(encodedChar);
+        }
+        textareaElements[0].value = '';
+        textareaElements[1].value = encodedMessage;
+    }
+
+    function result() {
+        textareaElements[1].value = receivedMessage;
+    }
 }
