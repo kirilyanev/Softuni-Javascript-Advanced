75b817ea6b6d6fb2af77d84f1f79e9533bfddd5c
Kiril Yanev <kl_yanev@abv.bg>
2023-02-04 23:03:16 +0200

    05-Command-processor

--- 13-Exercise-Advanced-Functions/02-Add.js
+++ 13-Exercise-Advanced-Functions/02-Add.js
@@ -1,9 +1,18 @@
-function solution(num) {
-    let number = num;
-    return function(num2) {
-        let result = number+num2;
-        return result;
+// First solution
+// function solution(num) {
+//     let number = num;
+//     return function(num2) {
+//         let result = number+num2;
+//         return result;
+//     }
+// }
+
+// Second solution
+function solution(number) {
+    function add(a, b) {
+        return a + b;
     }
+    return add.bind(this, number)
 }
 
 let add5 = solution(5);
@@ -13,3 +22,9 @@ console.log(add5(3));
 let add7 = solution(7);
 console.log(add7(2));
 console.log(add7(3));
+function solution(number) {
+    function add(a, b) {
+        return a + b;
+    }
+    return add.bind(this, number)
+}
