6a135574263a109a6d81ea54cb5ee27da7d003f6
Kiril Yanev <kl_yanev@abv.bg>
2023-02-04 21:06:12 +0200

    02-Add

--- 13-Exercise-Advanced-Functions/01-Area-and-Volume-Calculator.js
+++ 13-Exercise-Advanced-Functions/01-Area-and-Volume-Calculator.js
@@ -6,23 +6,23 @@ function vol() {
     return Math.abs(this.x * this.y * this.z);
 }
 
-function solve(area,vol,input) {
+function solve(area, vol, input) {
     let result = [];
     let coordinates = JSON.parse(input);
 
     for (let points of coordinates) {
-        result.push( {
+        result.push({
             area: area.call(points),
-            volume: vol.call(points)
+            volume: vol.call(points),
         });
-        
+
     }
-    console.log(result);
+    return result;
 }
 
 
-solve(area, vol, `[
+console.log(solve(area, vol, `[
     {"x":"1","y":"2","z":"10"},
     {"x":"7","y":"7","z":"10"},
     {"x":"5","y":"2","z":"10"}
-    ]`);
+    ]`));
