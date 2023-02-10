9642b9af199d982b7f5821b14284c9c53c0238c7
Kiril Yanev <kl_yanev@abv.bg>
2023-02-10 23:51:56 +0200

    05-Notification

--- 16-Exercise-Unit-Testing-and-Error-Handling/05.Notification/app.js
+++ 16-Exercise-Unit-Testing-and-Error-Handling/05.Notification/app.js
@@ -1,4 +1,14 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  let divElement = document.querySelector('#notification');
+  let paragraphElement = document.createElement('p');
+  divElement.addEventListener('click', onclick);
+
+  paragraphElement.textContent = message;
+  divElement.appendChild(paragraphElement);
+  divElement.style.display = 'block';
+
+  function onclick() {
+    divElement.style.display = 'none';
+    paragraphElement.remove()
+  }
 }
