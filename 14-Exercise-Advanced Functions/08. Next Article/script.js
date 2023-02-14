82ed212c2997c20c34d19865ce73c3a704b02fa9
Kiril Yanev <kl_yanev@abv.bg>
2023-02-14 18:12:15 +0200

    08-Next Article

--- 14-Exercise-Advanced Functions/08. Next Article/script.js
+++ 14-Exercise-Advanced Functions/08. Next Article/script.js
@@ -1,3 +1,17 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const content = document.querySelector('#content');
+    let arrayOfArticles = articles;
+
+    function showNext() {
+        let article = arrayOfArticles.shift();
+        
+        if(!article) {
+            return;
+        }
+        
+        const articleElement = document.createElement("article");
+        articleElement.textContent = article;
+        content.appendChild(articleElement);
+    }
+    return showNext;
 }
